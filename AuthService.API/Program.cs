18ba1fe0719e782afe4b41c22d838b31c80498d3
VMahluza <mahluzavictor@gmail.com>
2025-11-22 10:27:15 -0800

    Add MySQL support and new Infrastructure project
    - Added `MySqlConnector` package to `AuthService.API` for MySQL connectivity.
    - Configured `MySqlConnection` as a transient service in `Program.cs`.
    - Added a `Default` connection string in `appsettings.Development.json`.
    - Introduced `AuthService.Infrastructure` project for database and repository logic.
    - Set up `AuthService.Infrastructure` with .NET 10.0, Dapper, and DI support.
    - Added `DependencyInjection` class for service registration.
    - Updated solution file to include the new `AuthService.Infrastructure` project.

--- AuthService.API/Program.cs
+++ AuthService.API/Program.cs
@@ -1,11 +1,18 @@
+using MySqlConnector;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddSwaggerGen();  // Added for Swagger UI
+// SQL Connection 
+builder.Services.AddTransient(x =>
+  new MySqlConnection(builder.Configuration.GetConnectionString("Default")));
+
 
 var app = builder.Build();
 
---
+++ AuthService.Infrastructure/DependancyInjection.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AuthService.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddTransient(x =>
+          new MySqlConnection(configuration.GetConnectionString("Default")));
+        return services;
+
+
+
+    }
+}
