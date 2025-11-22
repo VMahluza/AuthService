d7285932d54bf5eeea57643c5f4c997c03375c1f
VMahluza <mahluzavictor@gmail.com>
2025-11-22 02:01:33 -0800

    Refactor User entity and introduce BaseEntity
    Refactored the `User` class to inherit from a new `BaseEntity`
    class, standardizing common properties like `Id`, `CreatedAt`,
    and `LastUpdatedAt`. Removed `FirstName` and `LastName`
    properties, along with the `FullName` computed property.
    
    Added `UserStatus` (enum-based) and `FailedLoginAttempts`
    properties to the `User` class for better user state tracking
    and security. Updated the `User` constructor and `RegisterNew`
    factory method to reflect these changes, including default
    initialization of `UserStatus` and timestamps.
    
    Replaced the `UserStatus` class with an enum for simplicity.
    Removed the `UserStatus` file. Updated `using` directives to
    include new namespaces. These changes improve code reuse,
    consistency, and maintainability.

---
+++ AuthService.Domain/Entities/Common/BaseEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AuthService.Domain.Entities.Common;
+
+public abstract class BaseEntity
+{
+    public Guid Id { get; protected set; }
+    public DateTime CreatedAt { get; protected set; } 
+    public DateTime? LastUpdatedAt { get; set; }
+
+}
--- AuthService.Domain/Entities/User/User.cs
+++ AuthService.Domain/Entities/User/User.cs
@@ -1,34 +1,40 @@
-﻿using AuthService.Domain.Interfaces;
+﻿using AuthService.Domain.Entities.Common;
+using AuthService.Domain.Enums;
+using AuthService.Domain.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace AuthService.Domain.Entities.User;
-public class User
+public class User : BaseEntity
 {
-    public Guid Id { get; private set; }
-    public string FirstName { get; private set; }
-    public string LastName { get; private set; }
     public string UserName { get; private set; }
     public EmailAddress Email { get; private set; }
     public PasswordHash PasswordHash { get; private set; }
+    public UserStatus Status { get; private set; }
 
-    public string FullName => $"{FirstName} {LastName}";
+    public int FailedLoginAttempts { get; private set; }
+
     private User() { }
-    public User(string firstName, string lastName, string userName, EmailAddress email, PasswordHash passwordHash)
+    public User(string userName, EmailAddress email, PasswordHash passwordHash, UserStatus status, int failedLoginAttempts)
     {
         Id = Guid.NewGuid();
-        FirstName = firstName;
-        LastName = lastName;
         UserName = userName;
         Email = email;
         PasswordHash = passwordHash;
+        Status = status;
+        FailedLoginAttempts = failedLoginAttempts;
     }
 
-    public static User RegisterNew(string firstName, string lastName, string userName, string email, string rawPassword, IPasswordHasher hasher)
+    public static User RegisterNew(string userName, string email, string rawPassword, IPasswordHasher hasher)
     {
         var emailAddress = EmailAddress.Create(email);
         var passwordHash = PasswordHash.Create(hasher.Hash(rawPassword).ToString());
-        return new User(firstName, lastName, userName, emailAddress, passwordHash);
+
+        return new User(userName, emailAddress, passwordHash, UserStatus.PendingVerification, 0)
+        {
+            CreatedAt = DateTime.UtcNow,
+            LastUpdatedAt = DateTime.UtcNow
+        };
     }
 }
