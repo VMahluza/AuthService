181b1b91dda93ac27fee7cea6442194881de2159
VMahluza <mahluzavictor@gmail.com>
2025-11-22 04:13:38 -0800

    Add generic and user-specific repository interfaces
    Introduced `IRepository<T>` to standardize CRUD operations for
    entities inheriting from `BaseEntity`. Added `IUserRepository`
    extending `IRepository<T>` with user-specific methods like
    `GetByEmailAsync`, `GetByUsernameAsync`, and `ExistsAsync`.
    
    Included necessary `using` directives and established a clear
    namespace structure (`AuthService.Domain.Interfaces.Repositories`)
    to improve code organization and maintainability.

---
+++ AuthService.Domain/Interfaces/Repositories/IRepositoy.cs
@@ -0,0 +1,12 @@
+﻿using AuthService.Domain.Entities.Common;
+
+namespace AuthService.Domain.Interfaces.Repositories;
+
+public interface IRepository<T> where T : BaseEntity
+{
+    Task AddAsync(T entity);
+    Task UpdateAsync(T entity);
+    Task DeleteAsync(T entity);
+    Task<T> GetByIdAsync(Guid id);
+    Task<IEnumerable<T>> GetPagedAsync(int pageNumber, int pageSize);
+}
---
+++ AuthService.Domain/Interfaces/Repositories/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using AuthService.Domain.Entities.User;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AuthService.Domain.Interfaces.Repositories;
+public interface IUserRepository : IRepository<User>
+{
+    Task<User> GetByEmailAsync(string email);
+    Task<User> GetByUsernameAsync(string username);
+    Task<bool> ExistsAsync(string email, string username);
+}
